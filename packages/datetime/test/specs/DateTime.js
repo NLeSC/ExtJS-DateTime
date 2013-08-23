d19046df858511ec4c6e5de493f616e985c4245d
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2013-08-23 17:02:06 +0200

    Added first test using Jasmine and ExtSpec.

---
+++ packages/datetime/test/specs/DateTime.js
@@ -0,0 +1,26 @@
+describe('NLeSC.ee.form.field.DateTime', function() {
+    'use strict';
+
+    var instance, esj = ExtSpec.Jasmine;
+
+    beforeEach(function() {
+        this.addMatchers(esj.Matchers);
+        instance = ExtSpec.create('NLeSC.ee.form.field.DateTime', function() {
+            this.callParent = jasmine.createSpy('callParent');
+            ExtSpec.Jasmine.createConfigSpies(this);
+        });
+    });
+
+    describe("focus", function() {
+        beforeEach(function() {
+            instance.dateField = jasmine.createSpyObj('dateField', ['focus']);
+        });
+        
+        it('focuses date field', function() {
+
+            instance.focus();
+
+            expect(instance.dateField.focus).toHaveBeenCalled();
+        });
+    });
+});
